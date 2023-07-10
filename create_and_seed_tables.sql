23ac58ec3a5e2113c9b7e1a69adc0670d6ac5aeb
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-07-10 23:56:50 +0200

    Initial sqlite setup instructions and database seeding

---
+++ create_and_seed_tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE user (
+	id INTEGER PRIMARY KEY,
+   	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT NOT NULL
+);
+
+INSERT INTO user SELECT
+  json_extract(value, '$.id'),
+  json_extract(value, '$.first_name'),
+  json_extract(value, '$.last_name'),
+  json_extract(value, '$.password'),
+  json_extract(value, '$.email')
+FROM json_each(readfile('user_seed.json'));
+
+CREATE TABLE empty_leg (
+	id INTEGER PRIMARY KEY,
+  departure TEXT NOT NULL,
+	destination TEXT NOT NULL,
+	departure_date INTEGER NOT NULL,
+	max_seats INTEGER NOT NULL,
+	available_seats INTEGER NOT NULL,
+	price INTEGER NOT NULL,
+	currency TEXT NOT NULL -- Three letter code as of https://www.xe.com/iso4217.php
+);
+
+INSERT INTO empty_leg SELECT
+  json_extract(value, '$.id'),
+  json_extract(value, '$.departure'),
+  json_extract(value, '$.destination'),
+  json_extract(value, '$.departure_date'),
+  json_extract(value, '$.max_seats'),
+  json_extract(value, '$.available_seats'),
+  json_extract(value, '$.price'),
+  json_extract(value, '$.currency')
+FROM json_each(readfile('empty_leg_seed.json'));
---
+++ sqlite.md
@@ -0,0 +1,38 @@
+# install sqlite
+Follow the instructions for your operating system on https://www.prisma.io/dataguide/sqlite/setting-up-a-local-sqlite-database.
+
+All commands assume they are run from this project directory.
+
+On windows all commands start with `sqlite3.exe` instead of `sqlite3`.
+
+# create database
+```bash
+sqlite3 skyhigh_dev.db < create_and_seed_tables.sql
+```
+
+# export data to json
+```bash
+sqlite3 skyhigh_dev.sqlite '.mode json' '.once user_seed.json' 'select * from user'
+```
+
+# import data from json
+Open the database with the sqlite CLI:
+```bash
+sqlite3 skyhigh_dev.db
+```
+A prompt like this should open:
+```bash
+SQLite version 3.42.0 2023-05-16 12:36:15
+Enter ".help" for usage hints.
+sqlite> 
+```
+Insert the following SQL and press Enter. To exit type `.quit`:
+```sql
+INSERT INTO user SELECT
+  json_extract(value, '$.id'),
+  json_extract(value, '$.first_name'),
+  json_extract(value, '$.last_name'),
+  json_extract(value, '$.password'),
+  json_extract(value, '$.email')
+FROM json_each(readfile('user_seed.json'));
+```
