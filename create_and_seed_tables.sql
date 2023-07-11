b14c8f685e2d60fef4b9e14b8db2a251d0533ef6
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-07-11 13:18:17 +0200

    Add booking with seed
    Also adding newlines to the end of files + a user_id to proposed_trip

--- create_and_seed_tables.sql
+++ create_and_seed_tables.sql
@@ -43,12 +43,32 @@ CREATE TABLE proposed_trip (
 	destination TEXT NOT NULL,
 	departure_date INTEGER NOT NULL,
   reservation_open INTEGER DEFAULT TRUE,
-	no_of_passengers INTEGER DEFAULT 1
+	no_of_passengers INTEGER DEFAULT 1,
+  user_id INTEGER NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(id)
 );
 
-INSERT INTO proposed_trip(id, departure, destination, departure_date) SELECT
+INSERT INTO proposed_trip(id, departure, destination, departure_date, user_id) SELECT
   json_extract(value, '$.id'),
   json_extract(value, '$.departure'),
   json_extract(value, '$.destination'),
-  json_extract(value, '$.departure_date')
+  json_extract(value, '$.departure_date'),
+  json_extract(value, '$.user_id')
 FROM json_each(readfile('proposed_trip_seed.json'));
+
+CREATE TABLE booking (
+	id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  empty_leg_id INTEGER DEFAULT NULL,
+  proposed_trip_id INTEGER DEFAULT NULL,
+  FOREIGN KEY(user_id) REFERENCES user(id),
+  FOREIGN KEY(empty_leg_id) REFERENCES empty_leg(id),
+  FOREIGN KEY(proposed_trip_id) REFERENCES proposed_trip(id)
+);
+
+INSERT INTO booking SELECT
+  json_extract(value, '$.id'),
+  json_extract(value, '$.user_id'),
+  json_extract(value, '$.empty_leg_id'),
+  json_extract(value, '$.proposed_trip_id')
+FROM json_each(readfile('booking_seed.json'));
