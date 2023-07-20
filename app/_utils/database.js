9d438878142e8223253734b9e98424b19a962a7f
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-07-20 20:44:55 +0200

    Revert "Initial propose trip page with server actions"
    This reverts commit 35225cdb395d4bd3bea8cd8adc7cb515314e0142.

--- app/_utils/database.js
+++
@@ -1,5 +0,0 @@
-// global singleton for the database
-// https://nextjs.org/docs/getting-started/react-essentials#sharing-data-between-server-components
-
-export const db = require('better-sqlite3')('skyhigh_dev.sqlite'/* , {verbose: console.log} */);
-db.pragma('journal_mode = WAL');
