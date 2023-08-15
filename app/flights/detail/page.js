483643d6bce7cdfed653442656a899d8fc863867
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-08-15 22:58:04 +0200

    Fix several warnings
    Warning: Invalid DOM property `abc-def`. Did you mean `abcDef`?

--- app/flights/detail/page.js
+++ app/flights/detail/page.js
@@ -78,7 +78,7 @@ function Flights() {
 
                             <span className="text-gray-400 bg-gray-200 rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 12H6" />
                                 </svg>
                             </span>
                         </button>
@@ -96,7 +96,7 @@ function Flights() {
 
                             <span className="text-gray-400 bg-gray-200 rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 12H6" />
                                 </svg>
                             </span>
                         </button>
@@ -113,7 +113,7 @@ function Flights() {
 
                         <span className="text-gray-400 bg-gray-200 rounded-full">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 12H6" />
                             </svg>
                         </span>
                     </button>
