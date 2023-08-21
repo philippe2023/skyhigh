6435f40e917f2c02f081f3ac1ed86aff169819a9
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-08-21 22:45:02 +0200

    Add dynamic data to profile pages
    only visible to logged in users.

--- app/flights/[id]/page.js
+++ app/flights/[id]/page.js
@@ -45,7 +45,7 @@ function Flights({params}) {
                 <div className=" lg:flex">
                     <div className="w-full lg:w-1/2">
                         <div className="lg:max-w-lg">
-                            <h1 className="text-xl font-semibold text-gray-800 dark:text-white lg:text-2xl">Flight proposed by <span className="text-blue-500 ">Jack Harlow</span></h1>
+                            <h1 className="text-xl font-semibold text-gray-800 dark:text-white lg:text-2xl">Empty Leg by <span className="text-blue-500 ">FlyAway LLC</span></h1>
                             <p className="mt-3 text-gray-600 dark:text-gray-400">{flightData.available_seats} {flightData.available_seats === 1 ? 'Seat' : 'Seats'} available - 1 Flight attendant - 2h Flight</p>
                             <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Contact the host</button>
                         </div>
--- app/user/profile/page.js
+++ app/user/profile/page.js
@@ -1,13 +1,57 @@
-function Profile() {
+import {db} from "../../_utils/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+
+function getBookingsByUser(id) {
+    const bookings = db.prepare(`
+        SELECT
+            booking.id as id, departure, destination,
+            substr('JanFebMarAprMayJunJulAugSepOctNovDec', 1 + 3*STRFTIME('%m', departure_date, 'unixepoch'), -3) || STRFTIME(' %d, %Y', departure_date, 'unixepoch')  AS date
+        FROM booking
+        JOIN
+            proposed_trip ON booking.proposed_trip_id=proposed_trip.id
+        WHERE
+            booking.user_id=?
+        UNION ALL
+        SELECT
+            booking.id as id, departure, destination,
+            substr('JanFebMarAprMayJunJulAugSepOctNovDec', 1 + 3*STRFTIME('%m', departure_date, 'unixepoch'), -3) || STRFTIME(' %d, %Y', departure_date, 'unixepoch')  AS date
+        FROM booking
+        JOIN empty_leg ON booking.empty_leg_id=empty_leg.id
+        WHERE
+            booking.user_id=?
+    `).all(id,id);
+    return bookings;
+}
+
+async function Profile() {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return (
+            <div className="container mx-auto my-auto">
+                <div className="w-full bg-center bg-cover h-[30rem]" style={{backgroundImage: 'url(/images/Hero.png)'}}>
+                    <div className="flex items-center justify-center w-full h-full">
+                        <div className="text-center">
+                            <h1 className="text-3xl font-bold lg:text-4xl dark:text-gray-200">You need to be logged in to view this page.</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const bookings = getBookingsByUser(session.user.id);
+
     return (
         <div className="bg-white dark:bg-gray-900">
             <div className="container px-6 py-10 mx-auto">
             <div className="flex flex-col items-center p-8 transition-colors duration-300 transform cursor-pointer group hover:bg-blue-600 rounded-xl">
                 <img className="object-cover w-32 h-32 rounded-full ring-4 ring-gray-300" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
 
-                <h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white">John Doe</h1>
+                <h1 className="mt-4 text-2xl font-semibold text-gray-700 dark:text-white group-hover:text-white">{session.user.firstName} {session.user.lastName}</h1>
 
-                <p className="mt-2 text-gray-500 capitalize dark:text-gray-300 group-hover:text-gray-300">john.doe@mail.com</p>
+                <p className="mt-2 text-gray-500 dark:text-gray-300 group-hover:text-gray-300">{session.user.email}</p>
                 </div>
 
                 <div className="flex items-center justify-center">
@@ -48,7 +92,8 @@ function Profile() {
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                                        <tr>
+                                        {bookings.map((booking) => (
+                                            <tr key={booking.id}>
                                             <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                                 <div className="inline-flex items-center gap-x-3">
                                                     <div className="flex items-center gap-x-2">
@@ -59,8 +104,8 @@ function Profile() {
                                                         </div>
                                                         
                                                         <div>
-                                                            <h2 className="font-normal text-gray-800 dark:text-white ">To London</h2>
-                                                            <p className="text-xs font-normal text-gray-500 dark:text-gray-400">from Paris</p>
+                                                            <h2 className="font-normal text-gray-800 dark:text-white ">To {booking.destination}</h2>
+                                                            <p className="text-xs font-normal text-gray-500 dark:text-gray-400">from {booking.departure}</p>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -68,8 +113,8 @@ function Profile() {
                                             <td className="px-12 py-4 text-sm font-normal text-gray-700 whitespace-nowrap">
                                                 Confirmed
                                             </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Jan 4, 2023</td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Paris Charles de Gaules</td>
+                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{booking.date}</td>
+                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{booking.departure} Airport</td>
                                             <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">GolfStream 500</td>
                                             <td className="px-4 py-4 text-sm whitespace-nowrap">
                                                 <button className="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
@@ -79,68 +124,7 @@ function Profile() {
                                                 </button>
                                             </td>
                                         </tr>
-                                        <tr>
-                                            <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                                <div className="inline-flex items-center gap-x-3">
-                                                    <div className="flex items-center gap-x-2">
-                                                        <div className="flex items-center justify-center w-8 h-8 text-blue-500 bg-blue-100 rounded-full dark:bg-gray-800">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                                                            </svg>
-                                                        </div>
-                                                        
-                                                        <div>
-                                                            <h2 className="font-normal text-gray-800 dark:text-white ">To London</h2>
-                                                            <p className="text-xs font-normal text-gray-500 dark:text-gray-400">from Paris</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-12 py-4 text-sm font-normal text-gray-700 whitespace-nowrap">
-                                                Confirmed
-                                            </td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Jan 4, 2023</td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Paris Charles de Gaules</td>
-                                            <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">GolfStream 500</td>
-                                            <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                <button className="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                                                    </svg>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                            <div className="inline-flex items-center gap-x-3">
-                                                <div className="flex items-center gap-x-2">
-                                                    <div className="flex items-center justify-center w-8 h-8 text-blue-500 bg-blue-100 rounded-full dark:bg-gray-800">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                                                        </svg>
-                                                    </div>
-                                                    
-                                                    <div>
-                                                        <h2 className="font-normal text-gray-800 dark:text-white ">To London</h2>
-                                                        <p className="text-xs font-normal text-gray-500 dark:text-gray-400">from Paris</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className="px-12 py-4 text-sm font-normal text-gray-700 whitespace-nowrap">
-                                            Confirmed
-                                        </td>
-                                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Jan 4, 2023</td>
-                                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">Paris Charles de Gaules</td>
-                                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">GolfStream 500</td>
-                                        <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                            <button className="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                                                </svg>
-                                            </button>
-                                        </td>
-                                    </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
