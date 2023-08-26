ce26889c827a88d6812a05ef8a9cb3fa250dfc1f
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-08-26 14:29:21 +0200

    Add transparent loading image, still not optimal

--- app/components/navbar/Logo.js
+++ app/components/navbar/Logo.js
@@ -1,30 +1,45 @@
-'use client';
+'use client'
 
 import Image from "next/image";
 import Link from "next/link";
-import { useTheme } from 'next-themes'
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
 
 function Logo() {
+    const [mounted, setMounted] = useState(false);
+
+    // useEffect only runs on the client, so now we can safely show the UI
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        // 4x1 transparent gif to have the same aspect ratio as the logo
+        return (
+        <Link href="/">
+            <Image src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAABCAQAAABTNcdGAAAAC0lEQVR42mNkgAIAABIAAmXG3J8AAAAASUVORK5CYII=" alt="Logo" className="hidden md:block cursor-pointer" width={120} height={30} />
+        </Link>);
+    }
+
     const { resolvedTheme } = useTheme();
     let src;
-
     switch (resolvedTheme) {
-    case 'light':
-        src = '/images/skyhigh.png';
-        break;
-    case 'dark':
-        src = '/images/skyhigh-dark.png';
-        break;
-    default:
-        src = '/images/skyhigh.png';
-        break;
+        case 'light':
+            src = '/images/skyhigh.png';
+            break;
+        case 'dark':
+            src = '/images/skyhigh-dark.png';
+            break;
+        default:
+            src = 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAABCAQAAABTNcdGAAAAC0lEQVR42mNkgAIAABIAAmXG3J8AAAAASUVORK5CYII=';
+            break;
     }
 
     return (
         <Link href="/">
-            <Image src={src} alt="Logo" className="hidden md:block cursor-pointer" height="120" width="120" />
+            <Image src={src} alt="Logo" className="hidden md:block cursor-pointer" width={120} height={30} />
         </Link>
     );
 }
 
-export default Logo
+export default Logo;
