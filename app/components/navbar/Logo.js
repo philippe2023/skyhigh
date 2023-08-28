da9995af1097b7c487344a5cd503a8d3db996064
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-08-28 22:16:32 +0200

    Improve logo theme handling and fix link

--- app/components/navbar/Logo.js
+++ app/components/navbar/Logo.js
@@ -7,11 +7,13 @@ import { useTheme } from 'next-themes';
 
 function Logo() {
     const [mounted, setMounted] = useState(false);
-
+    const { resolvedTheme } = useTheme();
     // useEffect only runs on the client, so now we can safely show the UI
     useEffect(() => {
         setMounted(true);
     }, []);
+
+    let src;
 
     if (!mounted) {
         // 4x1 transparent gif to have the same aspect ratio as the logo
@@ -19,20 +21,18 @@ function Logo() {
         <Link href="/">
             <Image src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAABCAQAAABTNcdGAAAAC0lEQVR42mNkgAIAABIAAmXG3J8AAAAASUVORK5CYII=" alt="Logo" className="hidden md:block cursor-pointer" width={120} height={30} />
         </Link>);
-    }
-
-    const { resolvedTheme } = useTheme();
-    let src;
-    switch (resolvedTheme) {
-        case 'light':
-            src = '/images/skyhigh.png';
-            break;
-        case 'dark':
-            src = '/images/skyhigh-dark.png';
-            break;
-        default:
-            src = 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAABCAQAAABTNcdGAAAAC0lEQVR42mNkgAIAABIAAmXG3J8AAAAASUVORK5CYII=';
-            break;
+    } else {
+        switch (resolvedTheme) {
+            case 'light':
+                src = '/images/skyhigh.png';
+                break;
+            case 'dark':
+                src = '/images/skyhigh-dark.png';
+                break;
+            default:
+                src = 'data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAABCAQAAABTNcdGAAAAC0lEQVR42mNkgAIAABIAAmXG3J8AAAAASUVORK5CYII=';
+                break;
+        }
     }
 
     return (
