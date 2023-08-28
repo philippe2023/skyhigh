b1234d71402c208fe46fd29ab693dcff28d5503e
Sven Diederichs <22592421+zaurask@users.noreply.github.com>
2023-08-28 19:23:56 +0200

    Add empty legs, private jets and images
    for destinations and flights.

--- app/components/flight/emptyLegCard.js
+++ app/components/flight/emptyLegCard.js
@@ -11,7 +11,7 @@ function EmptyLegCard({city}) {
     return (
         <Link href={exploreLink}>
             <div className="w-full max-w-xs text-align cursor-pointer">
-                <div className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group hover:outline hover:outline-2 outline-offset-2 outline-slate-300 solid" style={{backgroundImage: `url(/images/destinations/${city.destination.toLowerCase()}.jpg)`}}>
+                <div className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group hover:outline hover:outline-2 outline-offset-2 outline-slate-300 solid" style={{backgroundImage: `url(/images/destinations/${city.destination.toLowerCase().replace(" ","_")}.jpg)`}}>
                     <div className="flex flex-col justify-between w-full h-full px-5 pb-4 transition-opacity duration-700 group-hover:backdrop-blur-sm group-hover:bg-slate-500/30">
                     <button className="mt-4 px-2 py-1 w-24 text-sm text-white capitalize font-medium transition-colors duration-300 transform bg-slate-900 rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">{city.date}</button>
                         <div>
